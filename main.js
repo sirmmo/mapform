4d2dbb760031d8e44b4fba444ac62c37264cee2b
Marco Montanari <sirmmo@gmail.com>
2018-04-19 17:56:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
 var map;
 var ajaxRequest;
-var key = '1OVQ1cJYJbHdzxvUW2sW_TdAcVpm6QfQYhB0KUyiyKf8';
+//var key = '1OVQ1cJYJbHdzxvUW2sW_TdAcVpm6QfQYhB0KUyiyKf8';
+var key = '1gVRoXiWSgHZO4eBN56VAAgHDwPKsAZnHt7_rRYX4xzM';
 var url = 'https://spreadsheets.google.com/feeds/list/'+key+'/od6/public/values?alt=json-in-script';
 var areas = {};
 
